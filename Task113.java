d3ccdcd4ef328a4bb04267a659c0516a2391432b
ruth-jazmine-espiso <ruthjazmineespiso@gmail.com>
2025-04-28 06:11:03 +0000

    Task113.java

---
+++ Task113.java
@@ -0,0 +1,10 @@
+public class Task113 {
+    public static void main(String[] args) {
+        int check_number = 10;
+        String message;
+        for (int i = 1; i < check_number; i++) {
+            message = (i%2 == 0) ? i + " is an even number" : i + " is an odd number";
+            System.out.println(message);
+        }
+    }
+}
